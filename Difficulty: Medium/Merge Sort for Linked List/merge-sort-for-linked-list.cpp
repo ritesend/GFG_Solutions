10a030035929ed12e7c8aa96840a3e3264aef771
Ritesen Dhar <101635863+ritesend@users.noreply.github.com>
2025-09-08 20:29:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
@@ -0,0 +1,42 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* mergeSort(Node* head) {
+        // code here
+        Node* temp=head;
+        int c=0;
+        while(temp!=NULL){
+            c++;
+            temp=temp->next;
+        }
+        
+        vector<int>arr(c);
+        temp=head;
+        
+        for(int i=0;i<c;i++){
+            arr[i]=temp->data;
+            temp=temp->next;
+        }
+        
+        sort(arr.begin(),arr.end());
+        temp=head;
+        
+        int j=0;
+        while(temp!=NULL){
+            temp->data=arr[j++];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
